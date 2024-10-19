8746bc477ed63866fad62adc6b9b534f5c05e3a6
cornerloan <connorlowe21@gmail.com>
2024-10-19 03:05:19 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,22 @@
 import "./style.css";
 
-const APP_NAME = "Hello@@@@";
+const APP_NAME = "Hello";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+const app_title = document.createElement("h1");
+app_title.innerHTML = "Welcome to my canvas";
+app.append(app_title);
+
+const canvas = document.createElement("canvas") as HTMLCanvasElement;
+canvas.width = 256;
+canvas.height = 256;
+canvas.id = "myCanvas";
+app.append(canvas);
+const ctx = canvas.getContext('2d');
+if(ctx){
+    ctx.fillStyle = "green";
+    ctx?.fillRect(0, 0, canvas.width, canvas.height);
+}
